a9a6c6b70997fbc9d2408ac5214deed5c32e8267
Chirag Patel <Chirag_Patel@hms.harvard.edu>
2023-07-24 23:09:10 -0400

    new tables

--- pipeline/db_paths.R
+++ pipeline/db_paths.R
@@ -1,4 +1,4 @@
 ## relative paths to databases
 
-path_to_nhanes <- '../nhanes_122322.sqlite'
-path_to_summary_stats <- '../pe_summary_stats.sqlite'
+path_to_nhanes <- '../db/nhanes_122322.sqlite'
+path_to_summary_stats <- '../db/pe_summary_stats.sqlite'
