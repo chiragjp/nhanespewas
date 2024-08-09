29a6ad583da880c2682bedb91c03a70e16aab6ce
Chirag Patel <Chirag_Patel@hms.harvard.edu>
2024-08-08 21:26:10 -0400

    stupidly parallel

--- pipeline/exwas_script_cmd.R
+++ pipeline/exwas_script_cmd.R
@@ -9,27 +9,42 @@ library(logger)
 source('db_paths.R')
 
 path_out <- '../out'
-to_do <- read_csv(ss_file) |> group_by(pvarname) |> count()
+ss_file <- '../select/sample_size_pe_category_060623.csv'
+to_do <- read_csv(ss_file) |> group_by(pvarname) |> count() |> ungroup()
 path_scripts_out <- '../scripts'
 cmd_path <- '../pipeline/exwas.R'
 use_sbatch <- TRUE
-use_input_exposure_files <- TRUE # this is to filter the exposure inputs to parallelize the job: see 
-
-to_do <- read_csv(ss_file) |> filter(pvarname == phenotype) |> group_by(evarname) |> summarize(total_n=sum(n), num_surveys = n()) |> mutate(pvarname = phenotype)
-to_do <- to_do |> filter(num_surveys >=2, total_n >= sample_size_threshold)
+use_input_exposure_files <- TRUE # this is to filter the exposure inputs to parallelize the job: see exwas_exposure_aux_files.R
+num_input_exposure_files <- 10
 
 
-main_out <- file.path(path_scripts_out, "srun_me.sh") 
+
+main_out <- file.path(path_scripts_out, "srun_me.sh")
+#print(nrow(to_do))
+
 cat("",file=main_out)
 for(ii in 1:nrow(to_do)) {
   pvar <- to_do |> slice(ii) |> pull(pvarname)
-  rcmd <- sprintf("Rscript %s -p %s -l %s -i %s -o %s", file.path(cmd_path) , pvar, ss_file, path_to_nhanes, path_out)
-  
-  if(use_sbatch) {
-    cat(sprintf("sbatch --exclude=compute-f-17-[09-16] -o %s/%s.out -p short -t 11:59:00 -c 1 --mem=10G --wrap=\"%s\"\n", file.path(path_out), pvar, rcmd),
-        file=main_out, append = T)
+  rcmds <- vector("list", 1)
+  if(use_input_exposure_files) {
+    rcmds <- vector("list", num_input_exposure_files)
+    for(i in 1:num_input_exposure_files) {
+      e_file <- sprintf("%i.csv", i)
+      rcmds[[i]] <- sprintf("Rscript %s -p %s -l %s -i %s -o %s -e %s", file.path(cmd_path) , pvar, ss_file, path_to_nhanes, path_out, file.path(path_scripts_out, e_file))
+    }
   } else {
-    cat(sprintf("%s\n", rcmd), file=main_out, append = T)
+    rcmds[[1]] <- sprintf("Rscript %s -p %s -l %s -i %s -o %s", file.path(cmd_path) , pvar, ss_file, path_to_nhanes, path_out)
+  }
+
+  if(use_sbatch) {
+    for(i in 1:length(rcmds)) {
+      cat(sprintf("sbatch --exclude=compute-f-17-[09-16] -o %s/%s_%i.out -p short -t 11:59:00 -c 1 --mem=10G --wrap=\"%s\"\n", file.path(path_out), pvar, i, rcmds[i]),
+          file=main_out, append = T)
+    }
+  } else {
+    for(i in 1:length(rcmds)) {
+      cat(sprintf("%s\n", rcmds[[i]]), file=main_out, append = T)
+    }
   }
 }
 
